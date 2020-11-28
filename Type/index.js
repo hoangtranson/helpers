1d103d3f34822732934b4ad4bb130ff1a6ca368d
Tran Son Hoang <hayden.tran.d@gmail.com>
2020-11-28 22:47:06 +0800

    feat: add isEmail

--- Type/index.js
+++ Type/index.js
@@ -1,14 +1,18 @@
 const TypeModule = () => {
-    const isArray = item => Array.isArray(item);
-    const isObject =  item => typeof item === 'object' && Array.isArray(item) === false && item !== null;
-    const isFunction = item => typeof item === 'function';
+  const regEmail = /.+@.+\..+/;
 
-    return {
-      isArray,
-      isObject,
-      isFunction
-    };
+  const isArray = _arr => Array.isArray(_arr);
+  const isObject = _object =>
+    typeof item === "object" && Array.isArray(_object) === false && _object !== null;
+  const isFunction = _func => typeof _func === "function";
+  const isEmail = _email => regEmail.test(val);
+
+  return {
+    isArray,
+    isObject,
+    isFunction,
+    isEmail,
   };
-  
-  module.exports = TypeModule();
-  
+};
+
+module.exports = TypeModule();
