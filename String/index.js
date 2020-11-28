919e6fd156fe66c1f9130e819293f826a9ee8313
Tran Son Hoang <hayden.tran.d@gmail.com>
2020-11-28 21:59:08 +0800

    test: add validate HTML for stripHTMLflags

--- String/index.js
+++ String/index.js
@@ -1,9 +1,19 @@
-const StringModule = (() => {
-    const stripHTMLTags = str => str.replace(/<[^>]*>/g, '');
-
-    return {
-        stripHTMLTags
+const StringModule = () => {
+  const stripHTMLTags = (str) => {
+    if (!isHTML(str)) {
+      throw new Error("Input is not HTML.");
     }
-});
+    return str.replace(/<[^>]*>/g, "");
+  };
+
+  const isHTML = (str) => !(str || '')
+  .replace(/<([^>]+?)([^>]*?)>(.*?)<\/\1>/ig, '')
+  .replace(/(<([^>]+)>)/ig, '')
+  .trim();
+
+  return {
+    stripHTMLTags,
+  };
+};
 
 module.exports = StringModule();
--- String/stripHTMLTags.test.js
+++ String/stripHTMLTags.test.js
@@ -5,12 +5,25 @@ const TestCases = [
     {
         input: "<h1>test</h1>",
         output: "test"
+    },
+    {
+        input: `<p>My mother has <span style="color:blue;font-weight:bold">blue</span> eyes and my father has <span style="color:darkolivegreen;font-weight:bold">dark green</span> eyes.</p>`,
+        output: "My mother has blue eyes and my father has dark green eyes."
+    },
+    {
+        input: "<h1>test<h1",
+        output: "Input is not HTML."
     }
 ];
 
 TestCases.forEach( item => {
     test(`stripHTMLTags ${item.input} should return ${item.output}`, t => {
-        const _output = stripHTMLTags(item.input);
-        t.is(_output, item.output);
+        try {
+            const _output = stripHTMLTags(item.input);
+            t.is(_output, item.output);
+        }catch(error) {
+            t.is(error.message, item.output);
+        }
+        
     })
 })
