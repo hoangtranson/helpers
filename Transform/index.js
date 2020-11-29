df61acb403c6cd66b50f68fd26243983869c1faf
Tran Son Hoang <hayden.tran.d@gmail.com>
2020-11-29 10:55:12 +0800

    add transform folder

---
+++ Transform/index.js
@@ -0,0 +1,9 @@
+const TransformModule = () => {
+    const toString = _value => !_value ? "" : _value.toString();
+  
+    return {
+        toString,
+    };
+  };
+  
+  module.exports = TransformModule();
